40e0a82ec0912015f02a1c6d2383f74aebdaf646
stkim92 <saga2293@naver.com>
2020-04-24 11:12:41 +0900

    build debug

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-export NODE_OPTIONS="--max-old-space-size=4096"
 
 module.exports = {
   someSidebar: {
